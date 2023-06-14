ece76785e42e0e519d5e0f985e044a8df2bccbf2
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-14 18:44:29 +0200

    Update client_row.php

--- php/components/client_row.php
+++ php/components/client_row.php
@@ -4,15 +4,18 @@
     <td><?php echo htmlspecialchars($client['societe']) ?></td>
     <td><?php echo htmlspecialchars($client['email']) ?></td>
     <td>
-        <?php
-        $stmt2 = $pdo->prepare("SELECT avatar FROM users WHERE id = ?");
-        $stmt2->execute([$client['commercial_id']]);
-        $commercial = $stmt2->fetch(PDO::FETCH_ASSOC);
-        if ($commercial && isset($commercial['avatar'])) {
-            echo "<img src='{$commercial['avatar']}' alt='Avatar' class='img-fluid rounded-circle' style='width: 30px; height: 30px;'>";
-        } else {
-            echo $client['commercial_id'];
-        }
-        ?>
+        <div class="position-relative">
+            <?php
+            $stmt2 = $pdo->prepare("SELECT avatar, nom FROM users WHERE id = ?");
+            $stmt2->execute([$client['commercial_id']]);
+            $commercial = $stmt2->fetch(PDO::FETCH_ASSOC);
+            if ($commercial && isset($commercial['avatar'])) {
+                echo "<img src='{$commercial['avatar']}' alt='Avatar' class='img-fluid rounded-circle' style='width: 30px; height: 30px;'>";
+                echo "<span class='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-secondary' style='z-index: 1; font-size: 12px; opacity: 0;'>" . htmlspecialchars($commercial['nom']) . "</span>";
+            } else {
+                echo $client['commercial_id'];
+            }
+            ?>
+        </div>
     </td>
 </tr>
