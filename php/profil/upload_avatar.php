67156624449efb98fe926501f4e23d1674fae887
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-14 18:31:50 +0200

    Update upload_avatar.php

--- php/profil/upload_avatar.php
+++ php/profil/upload_avatar.php
@@ -1,18 +1,17 @@
 <?php
 session_start();
-
 include 'db_connection.php';
 
 $user_id = $_GET['user_id']; // Récupérer l'identifiant de l'utilisateur depuis l'URL
 
 $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/avatars/";
-$target_file = $target_dir . basename($_FILES["avatar"]["name"]);
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-$check = getimagesize($_FILES["avatar"]["tmp_name"]);
-if($check !== false) {
+$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+if ($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
 } else {
@@ -27,14 +26,16 @@ if (file_exists($target_file)) {
 }
 
 // Check file size
-if ($_FILES["avatar"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if (
+    $imageFileType != "jpg" && $imageFileType != "png" &&
+    $imageFileType != "jpeg" && $imageFileType != "gif"
+) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
@@ -42,10 +43,10 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+    // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["avatar"]["name"])). " has been uploaded.";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
         $relative_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $target_file);
         echo '<img src="' . htmlspecialchars($relative_path) . '" alt="Uploaded Image">';
         $stmt = $pdo->prepare("UPDATE users SET avatar = ? WHERE id = ?");
