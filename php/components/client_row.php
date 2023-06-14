b2c4a3abd4978ebb12342baf5231b0cb19c845c3
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-14 18:50:04 +0200

    Update client_row.php

--- php/components/client_row.php
+++ php/components/client_row.php
@@ -5,11 +5,14 @@
     <td><?php echo htmlspecialchars($client['email']) ?></td>
     <td>
         <?php
-        $stmt2 = $pdo->prepare("SELECT avatar, nom FROM users WHERE id = ?");
+        $stmt2 = $pdo->prepare("SELECT avatar FROM users WHERE id = ?");
         $stmt2->execute([$client['commercial_id']]);
         $commercial = $stmt2->fetch(PDO::FETCH_ASSOC);
         if ($commercial && isset($commercial['avatar'])) {
+            echo "<div>";
             echo "<img src='{$commercial['avatar']}' alt='Avatar' class='img-fluid rounded-circle' style='width: 30px; height: 30px;' title='" . htmlspecialchars($commercial['nom']) . "'>";
+            echo "<p>" . htmlspecialchars($commercial['nom']) . "</p>";
+            echo "</div>";
         } else {
             echo $client['commercial_id'];
         }
