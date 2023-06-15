0651ef7cf169d4eb074c895358d116868e8c6cf2
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-15 22:57:16 +0200

    1.11.1

--- php/clients.php
+++ php/clients.php
@@ -71,113 +71,154 @@ $commerciaux = $stmt->fetchAll();
 </head>
 
 <body>
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            <?php include 'components/menu.php'; ?>
+            <!-- / Menu -->
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                <?php include 'components/navbar.php'; ?>
+                <!-- / Navbar -->
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Dashboard /</span> Clients</h4>
+                        <div class="card mb-4">
+                            <h5 class="card-header">Ajouter un Client</h5>
+                            <form action="add_client.php" method="post" class="card-body">
+                                <h6>1. Informations Générales</h6>
+                                <div class="row g-3">
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="nom">Nom</label>
+                                        <input type="text" id="nom" name="nom" class="form-control" placeholder="Dupont"
+                                            required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="prenom">Prénom</label>
+                                        <input type="text" id="prenom" name="prenom" class="form-control"
+                                            placeholder="Jean" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="societe">Société</label>
+                                        <input type="text" id="societe" name="societe" class="form-control"
+                                            placeholder="Dupont SA" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="siret">SIRET</label>
+                                        <input type="text" id="siret" name="siret" class="form-control"
+                                            placeholder="123 456 789 00012" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="email">Email</label>
+                                        <input type="email" id="email" name="email" class="form-control"
+                                            placeholder="jean.dupont@example.com" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="temps_engagement">Temps d'engagement
+                                            (mois)</label>
+                                        <input type="number" id="temps_engagement" name="temps_engagement"
+                                            class="form-control" required>
+                                    </div>
+                                </div>
 
-    <div class="card mb-4">
-        <h5 class="card-header">Ajouter un Client</h5>
-        <form action="add_client.php" method="post" class="card-body">
-            <h6>1. Informations Générales</h6>
-            <div class="row g-3">
-                <div class="col-md-6">
-                    <label class="form-label" for="nom">Nom</label>
-                    <input type="text" id="nom" name="nom" class="form-control" placeholder="Dupont" required>
+                                <hr class="my-4 mx-n4" />
+                                <h6>2. Adresse</h6>
+                                <div class="row g-3">
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="adresse">Adresse</label>
+                                        <input type="text" id="adresse" name="adresse" class="form-control"
+                                            placeholder="123 rue du Pont" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="ville">Ville</label>
+                                        <input type="text" id="ville" name="ville" class="form-control"
+                                            placeholder="Paris" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="code_postal">Code Postal</label>
+                                        <input type="text" id="code_postal" name="code_postal" class="form-control"
+                                            placeholder="75001" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="pays">Pays</label>
+                                        <input type="text" id="pays" name="pays" class="form-control"
+                                            placeholder="France" required>
+                                    </div>
+                                </div>
+
+                                <hr class="my-4 mx-n4" />
+                                <h6>3. Options et Commercial</h6>
+                                <div class="row g-3">
+                                    <?php foreach ($options as $option): ?>
+                                    <div class="col-md-4">
+                                        <input type="checkbox" id="option<?php echo $option['id']; ?>" name="options[]"
+                                            class="form-check-input" value="<?php echo $option['id']; ?>">
+                                        <label class="form-check-label"
+                                            for="option<?php echo $option['id']; ?>"><?php echo $option['nom']; ?></label>
+                                    </div>
+                                    <?php endforeach; ?>
+
+                                    <div class="col-md-6">
+                                        <label class="form-label" for="commercial_id">Commercial</label>
+                                        <select id="commercial_id" name="commercial_id" class="select2 form-select">
+                                            <?php foreach ($commerciaux as $commercial): ?>
+                                            <option value="<?php echo $commercial['id']; ?>">
+                                                <?php echo $commercial['prenom'] . ' ' . $commercial['nom']; ?>
+                                            </option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <hr class="my-4 mx-n4" />
+                                <div class="pt-4">
+                                    <button type="submit" class="btn btn-primary me-sm-3 me-1">Ajouter le
+                                        Client</button>
+                                    <button type="reset" class="btn btn-label-secondary">Annuler</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- Modal to add new record -->
+                        <div class="offcanvas offcanvas-end" id="add-new-record">
+                            <!-- Your modal content here -->
+                        </div>
+                        <!--/ DataTable with Buttons -->
+                        <hr class="my-5" />
+                        <!-- / Content -->
+                        <div class="content-backdrop fade"></div>
+                    </div>
+                    <!-- Content wrapper -->
                 </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="prenom">Prénom</label>
-                    <input type="text" id="prenom" name="prenom" class="form-control" placeholder="Jean" required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="societe">Société</label>
-                    <input type="text" id="societe" name="societe" class="form-control" placeholder="Dupont SA"
-                        required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="siret">SIRET</label>
-                    <input type="text" id="siret" name="siret" class="form-control" placeholder="123 456 789 00012"
-                        required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="email">Email</label>
-                    <input type="email" id="email" name="email" class="form-control"
-                        placeholder="jean.dupont@example.com" required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="temps_engagement">Temps d'engagement (mois)</label>
-                    <input type="number" id="temps_engagement" name="temps_engagement" class="form-control" required>
-                </div>
+                <!-- / Layout page -->
             </div>
+            <!-- Overlay -->
+            <div class="layout-overlay layout-menu-toggle"></div>
+            <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+            <div class="drag-target"></div>
+        </div>
+        <!-- / Layout wrapper -->
 
-            <hr class="my-4 mx-n4" />
-            <h6>2. Adresse</h6>
-            <div class="row g-3">
-                <div class="col-md-6">
-                    <label class="form-label" for="adresse">Adresse</label>
-                    <input type="text" id="adresse" name="adresse" class="form-control" placeholder="123 rue du Pont"
-                        required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="ville">Ville</label>
-                    <input type="text" id="ville" name="ville" class="form-control" placeholder="Paris" required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="code_postal">Code Postal</label>
-                    <input type="text" id="code_postal" name="code_postal" class="form-control" placeholder="75001"
-                        required>
-                </div>
-                <div class="col-md-6">
-                    <label class="form-label" for="pays">Pays</label>
-                    <input type="text" id="pays" name="pays" class="form-control" placeholder="France" required>
-                </div>
-            </div>
+        <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+        <script src="../assets/vendor/libs/popper/popper.js"></script>
+        <script src="../assets/vendor/js/bootstrap.js"></script>
+        <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-            <hr class="my-4 mx-n4" />
-            <h6>3. Options et Commercial</h6>
-            <div class="row g-3">
-                <?php foreach ($options as $option): ?>
-                <div class="col-md-4">
-                    <input type="checkbox" id="option<?php echo $option['id']; ?>" name="options[]"
-                        class="form-check-input" value="<?php echo $option['id']; ?>">
-                    <label class="form-check-label"
-                        for="option<?php echo $option['id']; ?>"><?php echo $option['nom']; ?></label>
-                </div>
-                <?php endforeach; ?>
+        <script src="../assets/vendor/js/menu.js"></script>
+        <!-- endbuild -->
 
-                <div class="col-md-6">
-                    <label class="form-label" for="commercial_id">Commercial</label>
-                    <select id="commercial_id" name="commercial_id" class="select2 form-select">
-                        <?php foreach ($commerciaux as $commercial): ?>
-                        <option value="<?php echo $commercial['id']; ?>">
-                            <?php echo $commercial['prenom'] . ' ' . $commercial['nom']; ?>
-                        </option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-            </div>
-            <hr class="my-4 mx-n4" />
-            <div class="pt-4">
-                <button type="submit" class="btn btn-primary me-sm-3 me-1">Ajouter le Client</button>
-                <button type="reset" class="btn btn-label-secondary">Annuler</button>
-            </div>
-        </form>
-    </div>
-    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../assets/vendor/js/bootstrap.js"></script>
-    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+        <!-- Vendors JS -->
+        <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
 
-    <script src="../assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
+        <!-- Main JS -->
+        <script src="../assets/js/main.js"></script>
 
-    <!-- Vendors JS -->
-    <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+        <!-- Page JS -->
+        <script src="../assets/js/dashboards-analytics.js"></script>
 
-    <!-- Main JS -->
-    <script src="../assets/js/main.js"></script>
-
-    <!-- Page JS -->
-    <script src="../assets/js/dashboards-analytics.js"></script>
-
-    <!-- Place this tag in your head or just before your close body tag. -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
