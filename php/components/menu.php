ab2ecc47eb291ea46b4e1a8251bbf7d95a6a30d5
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-16 00:15:20 +0200

    Update menu.php

--- php/components/menu.php
+++ php/components/menu.php
@@ -19,19 +19,15 @@
                 <div data-i18n="Analytics">Dashboard</div>
             </a>
         </li>
-
-        <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">Profil</span>
-        </li>
         <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-dock-top"></i>
-                <div data-i18n="Account Settings">Account test</div>
+                <div data-i18n="Alertes">Alertes</div>
             </a>
             <ul class="menu-sub">
                 <li class="menu-item">
                     <a href="pages-account-settings-account.html" class="menu-link">
-                        <div data-i18n="Account">Mon compte</div>
+                        <div data-i18n="Defaillance site client">Defaillance site client</div>
                     </a>
                 </li>
                 <li class="menu-item">
@@ -39,54 +35,113 @@
                         <div data-i18n="Notifications">Notifications</div>
                     </a>
                 </li>
-                <li class="menu-item">
-                    <a href="pages-account-settings-connections.html" class="menu-link">
-                        <div data-i18n="Connections">Clients</div>
-                    </a>
-                </li>
             </ul>
         </li>
+        <!-- Séparateur -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Vendeur</span></li>
+        <!-- Séparateur -->
         <li class="menu-item">
             <a href="javascript:void(0);" class="menu-link menu-toggle">
                 <i class="menu-icon tf-icons bx bx-cube-alt"></i>
-                <div data-i18n="Misc">Misc</div>
+                <div data-i18n="Fichier clients">Fichier clients</div>
             </a>
             <ul class="menu-sub">
                 <li class="menu-item">
-                    <a href="pages-misc-error.html" class="menu-link">
-                        <div data-i18n="Error">Error</div>
+                    <a href="../php/Listing_clients.php" class="menu-link">
+                        <div data-i18n="Listes clients">Liste des clients</div>
                     </a>
                 </li>
                 <li class="menu-item">
-                    <a href="pages-misc-under-maintenance.html" class="menu-link">
-                        <div data-i18n="Under Maintenance">Under Maintenance</div>
+                    <a href="../php/clients.php" class="menu-link">
+                        <div data-i18n="Ajout d'un client">Ajout d'un client</div>
                     </a>
                 </li>
             </ul>
         </li>
-        <!-- Components -->
-        <li class="menu-header small text-uppercase"><span class="menu-header-text">Outils</span></li>
-        <!-- Cards -->
+        <!-- Séparateur -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Production</span></li>
+        <!-- Séparateur -->
         <li class="menu-item">
-            <a href="../php/Listing_clients.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-collection"></i>
-                <div data-i18n="Clients">Clients</div>
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                <div data-i18n="Projets">Projets</div>
             </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Créer un projet">Créer un projet</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Listes des projets">Liste des projets</div>
+                    </a>
+                </li>
+            </ul>
         </li>
+        <!-- Séparateur -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Developpement</span></li>
+        <!-- Séparateur -->
         <li class="menu-item">
-            <a href="../php/auth-register-basic.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-collection"></i>
-                <div data-i18n="Ajout d'un utilisateur">Ajout d'un utilisateur</div>
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                <div data-i18n="Mes missions">Mes missions</div>
             </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Listes de mes projets">Consulter mes projets</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Ajout d'un client">Mes tâches</div>
+                    </a>
+                </li>
+            </ul>
         </li>
+        <!-- Séparateur -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Maintenance</span></li>
+        <!-- Séparateur -->
         <li class="menu-item">
-            <a href="../php/clients.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-collection"></i>
-                <div data-i18n="Ajout d'un client">Ajout d'un client</div>
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                <div data-i18n="Maitenance">Maitenance</div>
             </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Etat des sites">État des sites</div>
+                    </a>
+                </li>
+            </ul>
         </li>
-        <!-- User interface -->
-
+        <!-- Séparateur -->
+        <li class="menu-header small text-uppercase"><span class="menu-header-text">Paramètres</span></li>
+        <!-- Séparateur -->
+        <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-cube-alt"></i>
+                <div data-i18n="Paramètres">Paramètres</div>
+            </a>
+            <ul class="menu-sub">
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Liste utilisateurs">Liste utilisateurs</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Ajout d'un utilisateur">Ajout d'un utilisateur</div>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="../php/dashboard.php" class="menu-link">
+                        <div data-i18n="Transférer les clients">Transférer les clients</div>
+                    </a>
+                </li>
+            </ul>
+        </li>
 
     </ul>
 </aside>
