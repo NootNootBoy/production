9379bbd9c95cbae0572a16e378c0e1ac55b90da1
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-16 00:30:13 +0200

    Update menu.php

--- php/components/menu.php
+++ php/components/menu.php
@@ -1,6 +1,6 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
-        <a href="index.html" class="app-brand-link">
+        <a href="../php/dashboard.php" class="app-brand-link">
             <span class="app-brand-text demo menu-text fw-bolder ">Mindset</span>
         </a>
 
