0a89888cfa091b3c669ea7a3d4d24a796bc0c611
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-15 23:21:57 +0200

    Update Listing_clients.php

--- php/Listing_clients.php
+++ php/Listing_clients.php
@@ -132,107 +132,47 @@ $pdo = new PDO($dsn, $user, $pass, $opt);
                             <!-- / Content -->
                             <div class="content-backdrop fade"></div>
                         </div>
-                        <div class="offcanvas-body flex-grow-1">
-                            <form class="add-new-record pt-0 row g-2" id="form-add-new-record" onsubmit="return false">
-                                <div class="col-sm-12">
-                                    <label class="form-label" for="basicFullname">Full Name</label>
-                                    <div class="input-group input-group-merge">
-                                        <span id="basicFullname2" class="input-group-text"><i
-                                                class="bx bx-user"></i></span>
-                                        <input type="text" id="basicFullname" class="form-control dt-full-name"
-                                            name="basicFullname" placeholder="John Doe" aria-label="John Doe"
-                                            aria-describedby="basicFullname2" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <label class="form-label" for="basicPost">Post</label>
-                                    <div class="input-group input-group-merge">
-                                        <span id="basicPost2" class="input-group-text"><i
-                                                class="bx bxs-briefcase"></i></span>
-                                        <input type="text" id="basicPost" name="basicPost" class="form-control dt-post"
-                                            placeholder="Web Developer" aria-label="Web Developer"
-                                            aria-describedby="basicPost2" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <label class="form-label" for="basicEmail">Email</label>
-                                    <div class="input-group input-group-merge">
-                                        <span class="input-group-text"><i class="bx bx-envelope"></i></span>
-                                        <input type="text" id="basicEmail" name="basicEmail"
-                                            class="form-control dt-email" placeholder="john.doe@example.com"
-                                            aria-label="john.doe@example.com" />
-                                    </div>
-                                    <div class="form-text">You can use letters, numbers & periods</div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <label class="form-label" for="basicDate">Joining Date</label>
-                                    <div class="input-group input-group-merge">
-                                        <span id="basicDate2" class="input-group-text"><i
-                                                class="bx bx-calendar"></i></span>
-                                        <input type="text" class="form-control dt-date" id="basicDate" name="basicDate"
-                                            aria-describedby="basicDate2" placeholder="MM/DD/YYYY"
-                                            aria-label="MM/DD/YYYY" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <label class="form-label" for="basicSalary">Salary</label>
-                                    <div class="input-group input-group-merge">
-                                        <span id="basicSalary2" class="input-group-text"><i
-                                                class="bx bx-dollar"></i></span>
-                                        <input type="number" id="basicSalary" name="basicSalary"
-                                            class="form-control dt-salary" placeholder="12000" aria-label="12000"
-                                            aria-describedby="basicSalary2" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <button type="submit"
-                                        class="btn btn-primary data-submit me-sm-3 me-1">Submit</button>
-                                    <button type="reset" class="btn btn-outline-secondary"
-                                        data-bs-dismiss="offcanvas">Cancel</button>
-                                </div>
-                            </form>
-                        </div>
+
+                        <!-- Content wrapper -->
                     </div>
-                    <!-- Content wrapper -->
+                    <!-- / Layout page -->
                 </div>
-                <!-- / Layout page -->
+                <!-- Overlay -->
+                <div class="layout-overlay layout-menu-toggle"></div>
+                <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+                <div class="drag-target"></div>
             </div>
-            <!-- Overlay -->
-            <div class="layout-overlay layout-menu-toggle"></div>
-            <!-- Drag Target Area To SlideIn Menu On Small Screens -->
-            <div class="drag-target"></div>
-        </div>
-        <!-- / Layout wrapper -->
+            <!-- / Layout wrapper -->
 
-        <!-- Core JS -->
-        <!-- build:js assets/vendor/js/core.js -->
-        <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-        <script src="../../assets/vendor/libs/popper/popper.js"></script>
-        <script src="../../assets/vendor/js/bootstrap.js"></script>
-        <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+            <!-- Core JS -->
+            <!-- build:js assets/vendor/js/core.js -->
+            <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+            <script src="../../assets/vendor/libs/popper/popper.js"></script>
+            <script src="../../assets/vendor/js/bootstrap.js"></script>
+            <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-        <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-        <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-        <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+            <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
+            <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
+            <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
 
-        <script src="../../assets/vendor/js/menu.js"></script>
-        <!-- endbuild -->
+            <script src="../../assets/vendor/js/menu.js"></script>
+            <!-- endbuild -->
 
-        <!-- Vendors JS -->
-        <script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
-        <!-- Flat Picker -->
-        <script src="../../assets/vendor/libs/moment/moment.js"></script>
-        <script src="../../assets/vendor/libs/flatpickr/flatpickr.js"></script>
-        <!-- Form Validation -->
-        <script src="../../assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js"></script>
-        <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js"></script>
-        <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js"></script>
+            <!-- Vendors JS -->
+            <script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+            <!-- Flat Picker -->
+            <script src="../../assets/vendor/libs/moment/moment.js"></script>
+            <script src="../../assets/vendor/libs/flatpickr/flatpickr.js"></script>
+            <!-- Form Validation -->
+            <script src="../../assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js"></script>
+            <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js"></script>
+            <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js"></script>
 
-        <!-- Main JS -->
-        <script src="../../assets/js/main.js"></script>
+            <!-- Main JS -->
+            <script src="../../assets/js/main.js"></script>
 
-        <!-- Page JS -->
-        <script src="../../assets/js/tables-datatables-basic.js"></script>
+            <!-- Page JS -->
+            <script src="../../assets/js/tables-datatables-basic.js"></script>
 </body>
 
 </html>
