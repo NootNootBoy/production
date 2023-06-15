f9aa5bec9b43127b574bfb99eb3fec07afd61ae0
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-15 10:57:07 +0200

    1.09.3

--- php/components/client_row.php
+++ php/components/client_row.php
@@ -3,27 +3,28 @@
     <td><?php echo htmlspecialchars($client['nom']) ?></td>
     <td><?php echo htmlspecialchars($client['societe']) ?></td>
     <td><?php echo htmlspecialchars($client['email']) ?></td>
-    <?php
+    <td>
+        <?php
         $stmt2 = $pdo->prepare("SELECT avatar, nom FROM users WHERE id = ?");
         $stmt2->execute([$client['commercial_id']]);
         $commercial = $stmt2->fetch(PDO::FETCH_ASSOC);
         if ($commercial && isset($commercial['avatar'])) {
         ?>
-    <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-            class="avatar avatar-xs pull-up" title="<?php echo htmlspecialchars($commercial['nom']) ?>">
-            <img src="<?php echo htmlspecialchars($commercial['avatar']) ?>" alt="Avatar" class="rounded-circle" />
-        </li>
-    </ul>
-    <?php
+        <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                class="avatar avatar-xs pull-up" title="<?php echo htmlspecialchars($commercial['nom']) ?>">
+                <img src="<?php echo htmlspecialchars($commercial['avatar']) ?>" alt="Avatar" class="rounded-circle" />
+            </li>
+        </ul>
+        <?php
         } else {
             echo $client['commercial_id'];
         }
         ?>
+    </td>
     <td>
         <span class="badge bg-label-primary me-1"><?php echo htmlspecialchars($client['statut']) ?></span>
     </td>
-    <td>
     <td>
         <div class="dropdown">
             <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
