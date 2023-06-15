6225e4e2cb45b31a43f40cde3c064dc16bccff09
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-16 00:37:12 +0200

    Update navbar.php

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -421,7 +421,7 @@
                                 <div class="dropdown-divider"></div>
                             </li>
                             <li>
-                                <a class="dropdown-item" href="auth-login-cover.html" target="_blank">
+                                <a class="dropdown-item" href="../php/logout.php" target="_blank">
                                     <i class="bx bx-power-off me-2"></i>
                                     <span class="align-middle">Log Out</span>
                                 </a>
