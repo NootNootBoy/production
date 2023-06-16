e3f463442f82d6bfabe657b3889463932e64a689
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-16 17:19:09 +0200

    Update upload_avatar.php

--- php/profil/upload_avatar.php
+++ php/profil/upload_avatar.php
@@ -1,58 +1,64 @@
 <?php
 session_start();
-include 'db_connection.php';
+
+include '../db_connection.php';
 
 $user_id = $_SESSION['user_id'];
 
-$target_dir = $_SERVER['DOCUMENT_ROOT'] . "/assets/img/avatars/";
+$stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
+$stmt->execute(['id' => $user_id]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$username = $user['username'];
+
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
-$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-if ($check !== false) {
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
-} else {
+  } else {
     echo "File is not an image.";
     $uploadOk = 0;
+  }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
 }
 
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
 }
 
 // Allow certain file formats
-if (
-    $imageFileType != "jpg" && $imageFileType != "png" &&
-    $imageFileType != "jpeg" && $imageFileType != "gif"
-) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
-        $relative_path = str_replace($_SERVER['DOCUMENT_ROOT'], '', $target_file);
-        echo '<img src="' . htmlspecialchars($relative_path) . '" alt="Uploaded Image">';
-        $stmt = $pdo->prepare("UPDATE users SET avatar = ? WHERE id = ?");
-        $stmt->execute([$relative_path, $user_id]);
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    $avatar_path = $target_file;
+    $stmt = $pdo->prepare("UPDATE users SET avatar = :avatar WHERE id = :id");
+    $stmt->execute(['avatar' => $avatar_path, 'id' => $user_id]);
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
 }
 ?>
