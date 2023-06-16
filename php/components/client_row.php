3cd9ad2675a261c35fdeffbf444adf5bb16c71bf
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-16 11:06:27 +0200

    Update client_row.php

--- php/components/client_row.php
+++ php/components/client_row.php
@@ -2,7 +2,6 @@
     <td><?php echo htmlspecialchars($client['id']) ?></td>
     <td><?php echo htmlspecialchars($client['societe']) ?></td>
     <td><?php echo htmlspecialchars($client['nom']) ?></td>
-    <td><?php echo htmlspecialchars($client['email']) ?></td>
     <td><?php echo htmlspecialchars($client['phone_number']) ?></td>
     <td>
         <?php
