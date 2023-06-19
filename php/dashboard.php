f22fb30bdf34f7cb4e7088e07fe446e8587fc200
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-19 12:38:30 +0200

    Update dashboard.php

--- php/dashboard.php
+++ php/dashboard.php
@@ -62,7 +62,7 @@ if (isset($_SESSION['error_message'])) {
     <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../../assets/vendor/css/rtl/core-dark.css" class="template-customizer-core-css" />
     <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="../../assets/css/demo.css" />
 
