de4c01c2dd54c76089d90e5eb53337fc20088c85
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-19 12:36:10 +0200

    Update menu.php

--- php/components/menu.php
+++ php/components/menu.php
@@ -1,7 +1,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="/php/dashboard.php" class="app-brand-link">
-            <img src="/assets/img/favicon/Logo-long-blanc.png" alt="logo" />
+            <img src="/assets/img/favicon/Logo-long-blanc.png" alt="logo" style="width:150px;" />
         </a>
 
         <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
