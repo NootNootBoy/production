00f7f14ec659de2244e43185c17c844eb899d61b
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-21 12:07:08 +0200

    Update listing_missions.php

--- php/missions/listing_missions.php
+++ php/missions/listing_missions.php
@@ -1,14 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+$stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
+$stmt->execute(['id' => $_SESSION['user_id']]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-<body>
-    <p>liste des missions :</p>
-</body>
+if (!isset($_SESSION['user_id'])) {
+    die('User ID not set in session');
+}
 
-</html>
+$host = '176.31.132.185';
+$db   = 'vesqbc_producti_db';
+$user = 'vesqbc_producti_db';
+$pass = '7f-yp!QZWOg6_%49';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
+
+// Récupérer l'ID de l'utilisateur actuellement connecté
+$id_user = $_SESSION['user_id'];
+
+try {
+    // Préparer la requête SQL pour récupérer les missions en attente
+    $stmt = $pdo->prepare("SELECT * FROM Missions WHERE id_user = :id_user AND etat = 'en attente'");
+
+    // Lier les paramètres
+    $stmt->bindParam(':id_user', $id_user);
+
+    // Exécuter la requête
+    $stmt->execute();
+
+    // Récupérer les résultats
+    $missions = $stmt->fetchAll();
+
+    // Afficher les missions
+    foreach ($missions as $mission) {
+        echo "<h2>" . $mission['nom_mission'] . "</h2>";
+        echo "<form action='update_mission.php' method='post'>";
+        echo "<input type='hidden' name='id_mission' value='" . $mission['id_mission'] . "'>";
+        echo "<input type='submit' name='action' value='Accepter'>";
+        echo "<input type='submit' name='action' value='Refuser'>";
+        echo "</form>";
+    }
+} catch(PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+$pdo = null;
+?>
