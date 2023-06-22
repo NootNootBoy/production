a56616cd86bc1dad6b01ab47110e4238f201aa40
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-22 17:48:19 +0200

    Update listing_project.php

--- php/project/listing_project.php
+++ php/project/listing_project.php
@@ -136,7 +136,7 @@ try {
                                         SELECT 
                                             Projets.*, 
                                             clients.id AS client_id, clients.nom AS client_nom, clients.prenom AS client_prenom, clients.offre_id AS client_offre_id, clients.date_signature AS client_date_signature, clients.email AS client_email,
-                                            offres.nom AS offre_nom, offres.prix_mensuel AS offre_prix_mensuel, offres.longues_traines AS offre_longues_traines, offres.nombre_villes AS offre_nombre_villes,
+                                            offres.nom AS offre_nom, offres.prix_mensuel AS offre_prix_mensuel, offres.nombre_longues_traines AS offre_nombre_longues_traines, offres.nombre_villes AS offre_nombre_villes,
                                             missions.id_mission AS mission_id, missions.progression AS mission_progression, missions.date_acceptation AS mission_date_acceptation,
                                             COUNT(taches.id_tache) AS total_taches, SUM(taches.est_complete) AS taches_completees
                                         FROM Projets 
