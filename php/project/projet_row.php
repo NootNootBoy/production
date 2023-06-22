395b4c96d2cb683e14ad8a3d3b33b725e0fc0c52
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-22 11:35:46 +0200

    Update projet_row.php

--- php/project/projet_row.php
+++ php/project/projet_row.php
@@ -3,7 +3,7 @@
     <td><?php echo htmlspecialchars($projet['nom_projet']) ?></td>
     <td>
         <?php
-        $stmt2 = $pdo->prepare("SELECT avatar, nom FROM users WHERE id = ?");
+        $stmt2 = $pdo->prepare("SELECT nom FROM clients WHERE id = ?");
         $stmt2->execute([$projet['id_client']]);
         $cliennt = $stmt2->fetch(PDO::FETCH_ASSOC);
         if ($cliennt && isset($cliennt['avatar'])) {
