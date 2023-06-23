7e7795e0a96f1397f56a3c33f34d28bb30b35397
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-23 16:56:42 +0200

    Update navbar.php

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -373,7 +373,7 @@ $opt = [
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                        <img src="<?php echo htmlspecialchars($userAvatar) ?> alt class=" w-px-40 h-auto
+                        <img src="<?php echo htmlspecialchars($userAvatar) ?>" alt class=" w-px-40 h-auto
                             rounded-circle" />
                     </div>
                 </a>
