916bd58222cbadeb0706083db011c942af0eade1
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-23 10:54:14 +0200

    missions listing

---
+++ php/missions/listing_missions_new.php
@@ -0,0 +1,229 @@
+<?php
+$host = '176.31.132.185';
+$db   = 'vesqbc_producti_db';
+$user = 'vesqbc_producti_db';
+$pass = '7f-yp!QZWOg6_%49';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+    $pdo = new PDO($dsn, $user, $pass, $opt);
+
+    // Préparer la requête SQL pour récupérer tous les projets
+    $stmt = $pdo->prepare("SELECT * FROM Projets");
+    $stmt->execute();
+    $clientCount = 0; // Initialise la variable $clientCount à 0 avant la boucle
+    while ($client = $stmt->fetch()) {
+        $clientCount++; // Incrémente la variable $clientCount pour chaque client
+    }
+
+try {
+    // Préparer la requête SQL pour récupérer tous les clients
+    $stmt = $pdo->prepare("SELECT * FROM clients");
+    $stmt->execute();
+    $clients = $stmt->fetchAll();
+
+    // Préparer la requête SQL pour récupérer tous les projets
+    $stmt = $pdo->prepare("SELECT * FROM missions");
+    $stmt->execute();
+    $projets = $stmt->fetchAll();
+} catch(PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr" class="dark-style layout-menu-fixed" dir="ltr" data-theme="theme-default-dark"
+    data-assets-path="../../assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Clients List</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="../../assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../../assets/vendor/css/rtl/core-dark.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default-dark.css"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../../assets/css/demo.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/sweetalert2/sweetalert2.css" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css" />
+    <link rel="stylesheet" href="../../assets/vendor/libs/flatpickr/flatpickr.css" />
+    <!-- Row Group CSS -->
+    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css" />
+    <!-- Form Validation -->
+    <link rel="stylesheet" href="../../assets/vendor/libs/formvalidation/dist/css/formValidation.min.css" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="../../assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="../../assets/vendor/js/template-customizer.js"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../../assets/js/config.js"></script>
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            <?php include '../../php/components/menu.php'; ?>
+            <!-- / Menu -->
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+                <?php include '../../php/components/navbar.php'; ?>
+                <!-- / Navbar -->
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Dashboard /</span> Mes missions
+                        </h4>
+                        <div class="card">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h5 class="card-header">Listes des missions (<span
+                                        class="fw-bold text-primary"><?php echo $clientCount; ?></span>) :</h5>
+                            </div>
+                            <div class="table-responsive text-nowrap">
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nom</th>
+                                            <th>Progression</th>
+                                            <th>Etat</th>
+                                            <th>Date de démarrage</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="table-border-bottom-0">
+                                        <?php   
+                                        $stmt = $pdo->query('SELECT * FROM missions');
+
+                                        while ($mission = $stmt->fetch()) {
+                                            include 'mission_row.php';
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+
+                        <!--/ DataTable with Buttons -->
+                        <hr class="my-5" />
+                        <!-- / Content -->
+                    </div>
+                    <!-- Content wrapper -->
+                </div>
+                <!-- / Layout page -->
+            </div>
+            <!-- Overlay -->
+            <div class="layout-overlay layout-menu-toggle"></div>
+            <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+            <div class="drag-target"></div>
+        </div>
+        <!-- / Layout wrapper -->
+        <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            // Vérifie les paramètres dans l'URL
+            var urlParams = new URLSearchParams(window.location.search);
+            var userAdded = urlParams.get('userAdded');
+            var error = urlParams.get('error');
+            var errorMessage = urlParams.get('errorMessage');
+
+            // Si 'userAdded' est vrai, affiche l'alerte de succès
+            if (userAdded === 'true') {
+                Swal.fire({
+                    title: 'Bien joué!',
+                    text: "Le client a été ajouté avec succès!",
+                    icon: 'success',
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    },
+                    buttonsStyling: false
+                });
+            }
+            // Sinon, s'il y a une erreur, affiche l'alerte d'erreur
+            else if (error === 'true') {
+                Swal.fire({
+                    title: 'Erreur!',
+                    text: errorMessage,
+                    icon: 'error',
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    },
+                    buttonsStyling: false
+                });
+            }
+        });
+        </script>
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+        <script src="../../assets/vendor/libs/popper/popper.js"></script>
+        <script src="../../assets/vendor/js/bootstrap.js"></script>
+        <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+        <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
+        <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
+        <script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
+
+        <script src="../../assets/vendor/js/menu.js"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="../assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
+        <script src="../../assets/js/extended-ui-sweetalert2.js"></script>
+        <script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+        <!-- Flat Picker -->
+        <script src="../../assets/vendor/libs/moment/moment.js"></script>
+        <script src="../../assets/vendor/libs/flatpickr/flatpickr.js"></script>
+        <!-- Form Validation -->
+        <script src="../../assets/vendor/libs/formvalidation/dist/js/FormValidation.min.js"></script>
+        <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/Bootstrap5.min.js"></script>
+        <script src="../../assets/vendor/libs/formvalidation/dist/js/plugins/AutoFocus.min.js"></script>
+
+        <!-- Main JS -->
+        <script src="../../assets/js/main.js"></script>
+
+        <!-- Page JS -->
+        <script src="../../assets/js/tables-datatables-basic.js"></script>
+</body>
+
+</html>
---
+++ php/missions/mission_row.php
@@ -0,0 +1,22 @@
+<tr>
+    <td><?php echo htmlspecialchars($mission['id_mission ']) ?></td>
+    <td><?php echo htmlspecialchars($mission['nom_mission']) ?></td>
+    <td><?php echo htmlspecialchars($mission['progression']) ?>%</td>
+    <td><?php echo htmlspecialchars($mission['etat']) ?>%</td>
+    <td><?php echo htmlspecialchars($mission['date_acceptation']) ?>%</td>
+    <td>
+        <div class="dropdown">
+            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                <i class="bx bx-dots-vertical-rounded"></i>
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item"
+                    href="/php/components/details_projet.php?id=<?php echo $projet['id_projet']; ?>"><i
+                        class="bx bx-edit-alt me-1"></i> Voir la fiche</a>
+                <a class="dropdown-item"
+                    href="/php/components/details_projet.php?id=<?php echo $projet['id_projet']; ?>"><i
+                        class="bx bx-trash me-1"></i> Archiver</a>
+            </div>
+        </div>
+    </td>
+</tr>
