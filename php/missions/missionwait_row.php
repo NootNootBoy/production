df239bb6af69487c204c15bb4b1d2d81358f7902
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-23 16:16:10 +0200

    Update missionwait_row.php

--- php/missions/missionwait_row.php
+++ php/missions/missionwait_row.php
@@ -2,11 +2,16 @@
     <td><?php echo htmlspecialchars($missionWait['id_mission']) ?></td>
     <td><?php echo htmlspecialchars($missionWait['nom_mission']) ?></td>
     <td><?php echo htmlspecialchars($missionWait['nom']); ?></td>
-    <td><?php echo htmlspecialchars($missionWait['etat']) ?></td>
+    <td>
+        <div class="alert alert-warning" role="alert">
+            <?php echo htmlspecialchars($missionWait['etat']) ?>
+        </div>
+    </td>
+
     <td>
         <form action='update_mission.php' method='post'>
             <input type='hidden' name='id_mission' value='<?php echo $missionWait['id_mission'] ?>'>
-            <button type='submit' name='action' value='Accepter' class='btn btn-primary'>Accepter</button>
+            <button type='submit' name='action' value='Accepter' class='btn btn-primary'>Démarrer </button>
             <button type='submit' name='action' value='Refuser' class='btn btn-danger'>Refuser</button>
         </form>
     </td>
