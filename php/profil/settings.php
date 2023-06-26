58d3d6bdafbb91865bb70655dfe15a3c6f4c3957
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-26 11:01:15 +0200

    Update settings.php

--- php/profil/settings.php
+++ php/profil/settings.php
@@ -1,6 +1,18 @@
 <?php
 session_start();
-include 'db_connection.php';
+$host = '176.31.132.185';
+$db   = 'vesqbc_producti_db';
+$user = 'vesqbc_producti_db';
+$pass = '7f-yp!QZWOg6_%49';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
 if (isset($_SESSION['user_id'])) {
     $user_id = $_SESSION['user_id'];
