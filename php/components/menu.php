f577e51e40842a26948d8f3e2f546ea066f1f9c8
François-Xavier Fin <xavierdk@outlook.fr>
2023-06-26 11:25:04 +0200

    Update menu.php

--- php/components/menu.php
+++ php/components/menu.php
@@ -1,3 +1,9 @@
+<?php 
+
+$_SESSION['rang'] = $userRang;
+
+?>
+
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
     <div class="app-brand demo">
         <a href="/php/dashboard.php" class="app-brand-link">
