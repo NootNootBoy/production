a94491eb7e0b4e3321d293833422942d71b6903e
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-27 17:30:43 +0200

    Update user_row.php

--- php/components/user_row.php
+++ php/components/user_row.php
@@ -9,7 +9,7 @@
             if ($user['rang'] == 'developpeur') {
                 echo 'bg-label-primary';
             } elseif ($user['rang'] == 'commercial') {
-                echo 'bg-label-warning';
+                echo 'bg-label-info';
             } elseif ($user['rang'] == 'administrateur') {
                 echo 'bg-label-danger';
             } elseif ($user['rang'] == 'assistant') {
