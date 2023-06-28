9080d53a27688aaf90da9d3e3cb6a72a9a505da2
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-28 15:56:28 +0200

    Update mark_as_read.php

--- php/notifications/mark_as_read.php
+++ php/notifications/mark_as_read.php
@@ -1,6 +1,9 @@
 <?php
 include '../../php/db_connection.php';
 require_once 'notifications.php';
+session_start();
+$userId = $_SESSION['user_id']; // Remplacez 'user_id' par la clé appropriée si nécessaire
+
 if(isset($_POST['notification_id'])){
   markNotificationAsRead($_POST['notification_id'], $userId); // Remplacez $userId par l'ID de l'utilisateur actuellement connecté
   echo 'Notification marked as read';
