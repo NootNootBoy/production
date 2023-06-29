3fb4a63827c0818ddcdc7e06da52244ec2c4cfa4
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-29 14:44:38 +0200

    Update navbar.php

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -143,7 +143,7 @@
                         <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
                             data-bs-toggle="dropdown">
                             <div class="avatar avatar-online">
-                                <img src="../../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                                <img src="<?php echo $user['avatar']?>" alt class="w-px-40 h-auto rounded-circle" />
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
