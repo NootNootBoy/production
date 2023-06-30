070a6af62271e9b079fa1d2536bf23192860c73a
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-06-30 10:41:34 +0200

    Update Listing_clients.php

--- php/Listing_clients.php
+++ php/Listing_clients.php
@@ -265,20 +265,25 @@ $stmt = $pdo->query('SELECT * FROM clients ORDER BY created_at DESC');
                                                     <label class="form-label" for="commercial_id">Commercial</label>
                                                     <select id="commercial_id" name="commercial_id"
                                                         class="select2 form-select">
-                                                        <?php foreach ($commerciaux as $commercial): ?>
-                                                            <option value="<?php echo isset($commercial['id']) ? $commercial['id'] : ''; ?>">
-    <?php echo isset($commercial['prenom']) ? $commercial['prenom'] . ' ' . $commercial['nom'] : ''; ?>
-</option>
-                                                        <?php endforeach; ?>
+                                                        <?php 
+    var_dump($commerciaux);
+    foreach ($commerciaux as $commercial): 
+?>
+    <option value="<?php echo isset($commercial['id']) ? $commercial['id'] : ''; ?>">
+        <?php echo isset($commercial['prenom']) ? $commercial['prenom'] . ' ' . $commercial['nom'] : ''; ?>
+    </option>
+<?php 
+    endforeach; 
+?>
                                                     </select>
                                                 </div>
                                                 <div class="col-md-6">
                                                 <label class="form-label" for="second_commercial_id">Second Commercial</label>
                                                 <select id="second_commercial_id" name="second_commercial_id" class="select2 form-select">
                                                     <?php foreach ($commerciaux as $commercial): ?>
-                                                        <option value="<?php echo isset($commercial['id']) ? $commercial['id'] : ''; ?>">
-    <?php echo isset($commercial['prenom']) ? $commercial['prenom'] . ' ' . $commercial['nom'] : ''; ?>
-</option>
+                                                    <option value="<?php echo $commercial['id']; ?>">
+                                                        <?php echo $commercial['prenom'] . ' ' . $commercial['nom']; ?>
+                                                    </option>
                                                     <?php endforeach; ?>
                                                 </select>
                                             </div>
