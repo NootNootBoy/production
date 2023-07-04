50d1970897fc80b5d332a633ef56bf0d5d30d876
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-04 16:35:16 +0200

    Update user_row.php

--- php/components/user_row.php
+++ php/components/user_row.php
@@ -1,24 +1,35 @@
-<td><?php echo isset($user['id']) ? htmlspecialchars($user['id']) : '' ?></td>
-<td><?php echo isset($user['nom']) ? htmlspecialchars($user['nom']) : '' ?></td>
-<td><?php echo isset($user['prenom']) ? htmlspecialchars($user['prenom']) : '' ?></td>
-<td><?php echo isset($user['email']) ? htmlspecialchars($user['email']) : '' ?></td>
-<td>
-    <span class="badge 
+<tr>
+    <td><?php echo htmlspecialchars($user['id']) ?></td>
+    <td><?php echo htmlspecialchars($user['nom']) ?></td>
+    <td><?php echo htmlspecialchars($user['prenom']) ?></td>
+    <td><?php echo htmlspecialchars($user['email']) ?></td>
+    <td>
+        <span class="badge 
         <?php 
-            if (isset($user['rang']) && $user['rang'] == 'developpeur') {
+            if ($user['rang'] == 'developpeur') {
                 echo 'bg-label-primary';
-            } elseif (isset($user['rang']) && $user['rang'] == 'commercial') {
+            } elseif ($user['rang'] == 'commercial') {
                 echo 'bg-label-info';
-            } elseif (isset($user['rang']) && $user['rang'] == 'administrateur') {
+            } elseif ($user['rang'] == 'administrateur') {
                 echo 'bg-label-danger';
-            } elseif (isset($user['rang']) && $user['rang'] == 'assistant') {
+            } elseif ($user['rang'] == 'assistant') {
                 echo 'bg-label-success';
             }
         ?> me-1">
-        <?php echo isset($user['rang']) ? htmlspecialchars($user['rang']) : '' ?>
-    </span>
-</td>
-<td><?php echo isset($user['phone_number']) ? htmlspecialchars($user['phone_number']) : '' ?></td>
-<td><?php echo isset($user['agence']) ? htmlspecialchars($user['agence']) : '' ?></td>
-</td>
+            <?php echo htmlspecialchars($user['rang']) ?>
+        </span>
+    </td>
+    <td><?php echo htmlspecialchars($user['phone_number']) ?></td>
+    <td><?php echo htmlspecialchars($user['agence_id']) ?></td>
+    <td>
+        <div class="dropdown">
+            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                <i class="bx bx-dots-vertical-rounded"></i>
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+            </div>
+        </div>
+    </td>
 </tr>
