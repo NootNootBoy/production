ccc20076b4d329af036f4e9706da4ff78e599913
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-05 15:38:49 +0200

    Update dashboard.php

--- php/dashboard.php
+++ php/dashboard.php
@@ -331,9 +331,9 @@ if (!isset($_SESSION['username'])) {
                                         <td>
                                             <div class="d-flex align-items-center">
                                                 <?php if (!empty($avatar)) { ?>
-                                                    <img src="<?php echo htmlspecialchars($avatar); ?>" alt="Avatar" width="20px" class="rounded-circle" />
+                                                    <img src="<?php echo htmlspecialchars($avatar); ?>" alt="Avatar" width="20px" class="rounded-circle m-3" />
                                                 <?php } else { ?>
-                                                    <img src="/assets/img/avatars/1.png" alt="Avatar" width="20px" class="rounded-circle" />
+                                                    <img src="/assets/img/avatars/1.png" alt="Avatar" width="20px" class="rounded-circle m-3" />
                                                 <?php } ?>
                                                 <span><?php echo $username; ?></span>
                                             </div>
