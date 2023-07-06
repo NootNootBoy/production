ce3916e6d51fccfa4943e326035ece19d9c415b8
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-06 15:09:22 +0200

    Update dashboard.php

--- php/dashboard.php
+++ php/dashboard.php
@@ -20,47 +20,44 @@ if (!isset($_SESSION['username'])) {
     echo "<script>console.log('Connecté en tant que : " . $_SESSION['username'] . "');</script>";
 }
 
-    // Préparation et exécution de la requête pour le CA en prévision des 28 derniers jours
-    $stmt = $pdo->prepare('
+// Préparation et exécution de la requête pour le CA en prévision des 28 derniers jours
+$stmt = $pdo->prepare('
     SELECT SUM(CA.CA_prevision) AS CA_prevision_28_days
-    FROM users
-    LEFT JOIN CA ON users.id = CA.commercial_id OR users.id = CA.second_commercial_id
-    WHERE CA.CA_realise IS NULL AND users.id = :userId
+    FROM CA
+    WHERE (commercial_id = :userId OR second_commercial_id = :userId) AND CA.CA_realise IS NULL
 ');
 $stmt->execute(['userId' => $userId]);
 $CA_prevision_28_days = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_28_days'];
 
-    // Préparation et exécution de la requête pour le CA en prévision des 3 derniers mois
-    $stmt = $pdo->prepare('
-        SELECT SUM(CA_prevision) AS CA_prevision_3_months
-        FROM CA
-        WHERE commercial_id = :userId AND date_realisation >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
-    ');
-    $stmt->execute(['userId' => $userId]);
-    $CA_prevision_3_months = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_3_months'];
+// Préparation et exécution de la requête pour le CA en prévision des 3 derniers mois
+$stmt = $pdo->prepare('
+    SELECT SUM(CA.CA_prevision) AS CA_prevision_3_months
+    FROM CA
+    WHERE (commercial_id = :userId OR second_commercial_id = :userId) AND date_realisation >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
+');
+$stmt->execute(['userId' => $userId]);
+$CA_prevision_3_months = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_3_months'];
 
-    // Préparation et exécution de la requête pour le CA en prévision depuis le début
-    $stmt = $pdo->prepare('
-        SELECT SUM(CA_prevision) AS CA_prevision_total
-        FROM CA
-        WHERE commercial_id = :userId
-    ');
-    $stmt->execute(['userId' => $userId]);
-    $CA_prevision_total = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_total'];
+// Préparation et exécution de la requête pour le CA en prévision depuis le début
+$stmt = $pdo->prepare('
+    SELECT SUM(CA.CA_prevision) AS CA_prevision_total
+    FROM CA
+    WHERE commercial_id = :userId OR second_commercial_id = :userId
+');
+$stmt->execute(['userId' => $userId]);
+$CA_prevision_total = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_total'];
 
-    // Préparation et exécution de la requête pour le CA réalisé
-    $stmt = $pdo->prepare('
-        SELECT SUM(CA_realise) AS CA_realise
-        FROM CA
-        WHERE commercial_id = :userId
-    ');
-    $stmt->execute(['userId' => $userId]);
-    $CA_realise = $stmt->fetch(PDO::FETCH_ASSOC)['CA_realise'];
+// Préparation et exécution de la requête pour le CA réalisé
+$stmt = $pdo->prepare('
+    SELECT SUM(CA.CA_realise) AS CA_realise
+    FROM CA
+    WHERE commercial_id = :userId OR second_commercial_id = :userId
+');
+$stmt->execute(['userId' => $userId]);
+$CA_realise = $stmt->fetch(PDO::FETCH_ASSOC)['CA_realise'];
 
-    // Calcul du pourcentage de variation entre les 28 derniers jours et les 3 derniers mois
-    $variation_28_days_vs_3_months = ($CA_prevision_28_days - $CA_prevision_3_months) / $CA_prevision_3_months * 100;
-
-
+// Calcul du pourcentage de variation entre les 28 derniers jours et les 3 derniers mois
+$variation_28_days_vs_3_months = ($CA_prevision_28_days - $CA_prevision_3_months) / $CA_prevision_3_months * 100;
 
 ?>
 
@@ -282,68 +279,67 @@ $CA_prevision_28_days = $stmt->fetch(PDO::FETCH_ASSOC)['CA_prevision_28_days'];
                                 <th class="w-50">Objectif de vente (135.000€)</th>
                               </tr>
                             </thead>
-                            <tbody>
-                            <?php
-// Effectuer votre requête pour obtenir les chiffres d'affaires en prévision des commerciaux
-// et les trier par ordre décroissant
-$stmt = $pdo->prepare('
-    SELECT users.username, users.avatar, SUM(CA.CA_prevision) AS CA_prevision
-    FROM users
-    LEFT JOIN CA ON users.id = CA.commercial_id OR users.id = CA.second_commercial_id
-    WHERE CA.CA_realise IS NULL AND users.rang = "commercial"
-    GROUP BY users.username
-    ORDER BY CA_prevision DESC
-');
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                                <tbody>
+                                <?php
+                                    // Effectuer votre requête pour obtenir les chiffres d'affaires en prévision des commerciaux
+                                    // et les trier par ordre décroissant
+                                    $stmt = $pdo->prepare('
+                                        SELECT users.username, users.avatar, SUM(CA.CA_prevision) AS CA_prevision
+                                        FROM users
+                                        LEFT JOIN CA ON users.id = CA.commercial_id OR users.id = CA.second_commercial_id
+                                        WHERE CA.CA_realise IS NULL AND users.rang = "commercial"
+                                        GROUP BY users.username
+                                        ORDER BY CA_prevision DESC
+                                    ');
+                                    $stmt->execute();
+                                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// Boucle pour afficher les données de chaque commercial
-$position = 1;
-foreach ($result as $row) {
-    $username = $row['username'];
-    $avatar = $row['avatar'];
-    $CA_prevision = $row['CA_prevision'];
-    $progress = ($CA_prevision / 135000) * 100;
-    $progressColor = '';
+                                    // Boucle pour afficher les données de chaque commercial
+                                    $position = 1;
+                                    foreach ($result as $row) {
+                                        $username = $row['username'];
+                                        $avatar = $row['avatar'];
+                                        $CA_prevision = $row['CA_prevision'];
+                                        $progress = ($CA_prevision / 135000) * 100;
+                                        $progressColor = '';
 
-    // Déterminer la couleur de la barre de progression en fonction des seuils
-    if ($CA_prevision < 30000) {
-        $progressColor = 'bg-danger';
-    } elseif ($CA_prevision < 75000) {
-        $progressColor = 'bg-warning';
-    } else {
-        $progressColor = 'bg-success';
-    }
-    ?>
+                                        // Déterminer la couleur de la barre de progression en fonction des seuils
+                                        if ($CA_prevision < 30000) {
+                                            $progressColor = 'bg-danger';
+                                        } elseif ($CA_prevision < 75000) {
+                                            $progressColor = 'bg-warning';
+                                        } else {
+                                            $progressColor = 'bg-success';
+                                        }
+                                        ?>
 
-    <tr>
-        <td><?php echo $position; ?></td>
-        <td>
-            <div class="d-flex align-items-center">
-                <?php if (!empty($avatar)) { ?>
-                    <img src="<?php echo htmlspecialchars($avatar); ?>" alt="Avatar" width="30px" class="rounded-circle m-2" />
-                <?php } else { ?>
-                    <img src="/assets/img/avatars/1.png" alt="Avatar" width="30px" class="rounded-circle m-2" />
-                <?php } ?>
-                <span><?php echo $username; ?></span>
-            </div>
-        </td>
-        <td><?php echo number_format($CA_prevision, 0, '.', ' '); ?>€</td>
-        <td>
-            <div class="d-flex justify-content-between align-items-center gap-3">
-                <div class="progress w-100" style="height: 10px">
-                    <div class="progress-bar <?php echo $progressColor; ?>" role="progressbar" style="width: <?php echo $progress; ?>%" aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <small class="fw-semibold"><?php echo number_format($progress, 0, '.', ' '); ?>%</small>
-            </div>
-        </td>
-    </tr>
+                                        <tr>
+                                            <td><?php echo $position; ?></td>
+                                            <td>
+                                                <div class="d-flex align-items-center">
+                                                    <?php if (!empty($avatar)) { ?>
+                                                        <img src="<?php echo htmlspecialchars($avatar); ?>" alt="Avatar" width="30px" class="rounded-circle m-2" />
+                                                    <?php } else { ?>
+                                                        <img src="/assets/img/avatars/1.png" alt="Avatar" width="30px" class="rounded-circle m-2" />
+                                                    <?php } ?>
+                                                    <span><?php echo $username; ?></span>
+                                                </div>
+                                            </td>
+                                            <td><?php echo number_format($CA_prevision, 0, '.', ' '); ?>€</td>
+                                            <td>
+                                                <div class="d-flex justify-content-between align-items-center gap-3">
+                                                    <div class="progress w-100" style="height: 10px">
+                                                        <div class="progress-bar <?php echo $progressColor; ?>" role="progressbar" style="width: <?php echo $progress; ?>%" aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0" aria-valuemax="100"></div>
+                                                    </div>
+                                                    <small class="fw-semibold"><?php echo number_format($progress, 0, '.', ' '); ?>%</small>
+                                                </div>
+                                            </td>
+                                        </tr>
 
-    <?php
-    $position++;
-}
-?>
-
+                                        <?php
+                                        $position++;
+                                    }
+                                    ?>
                             </tbody>
                           </table>
                         </div>
