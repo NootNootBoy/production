bc43ca0fdc2146ae305f4e34bae73f98215d85c2
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-07 13:12:27 +0200

    Update classement_agences_prevision.php

--- php/components/classement_agences_prevision.php
+++ php/components/classement_agences_prevision.php
@@ -1,43 +1,42 @@
  <?php
-$stmt = $pdo->prepare('
-SELECT agences.nom AS agence_nom, SUM(CA.CA_prevision) AS CA_prevision, objectifs.objectif AS objectif
-FROM users
-LEFT JOIN CA ON users.id = CA.commercial_id OR users.id = CA.second_commercial_id
-JOIN agences ON users.agence_id = agences.id
-LEFT JOIN objectifs ON agences.id = objectifs.agence_id AND MONTH(objectifs.mois) = MONTH(CURRENT_DATE())
-JOIN clients ON CA.client_id = clients.id
-WHERE CA.CA_realise IS NULL AND clients.statut = "actif"
-GROUP BY agences.nom
-ORDER BY CA_prevision DESC
-');
+                            // Effectuer votre requête pour obtenir les chiffres d'affaires en prévision des agences
+                            // et les trier par ordre décroissant
+                            $stmt = $pdo->prepare('
+                            SELECT agences.nom AS agence_nom, SUM(CA.CA_prevision) AS CA_prevision
+                            FROM users
+                            LEFT JOIN CA ON users.id = CA.commercial_id OR users.id = CA.second_commercial_id
+                            JOIN agences ON users.agence_id = agences.id
+                            LEFT JOIN objectifs ON agences.id = objectifs.agence_id AND MONTH(objectifs.mois) = MONTH(CURRENT_DATE())
+                            JOIN clients ON CA.client_id = clients.id
+                            WHERE CA.CA_realise IS NULL AND clients.statut = "actif"
+                            GROUP BY agences.nom
+                            ORDER BY CA_prevision DESC
+                        ');
+                            $stmt->execute();
+                            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    
+                            // Boucle pour afficher les données de chaque agence
+                            $position = 1;
+                            foreach ($result as $row) {
+                                $agence_nom = $row['agence_nom'];
+                                $CA_prevision = $row['CA_prevision'];
+                                $progress = ($CA_prevision / 135000) * 100;
+                                $progressColor = '';
 
-$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// Boucle pour afficher les données de chaque agence
-$position = 1;
-foreach ($result as $row) {
-$agence_nom = $row['agence_nom'];
-$CA_prevision = $row['CA_prevision'];
-$objectif = $row['objectif']; // Ajout de l'objectif
-$progress = ($CA_prevision / $objectif) * 100; // Calcul du pourcentage par rapport à l'objectif
-$progressColor = '';
-
-// Déterminer la couleur de la barre de progression en fonction des seuils
-if ($CA_prevision < 30000) {
-    $progressColor = 'bg-danger';
-} elseif ($CA_prevision < 75000) {
-    $progressColor = 'bg-warning';
-} else {
-    $progressColor = 'bg-success';
-}
-?>
+                                // Déterminer la couleur de la barre de progression en fonction des seuils
+                                if ($CA_prevision < 30000) {
+                                    $progressColor = 'bg-danger';
+                                } elseif ($CA_prevision < 75000) {
+                                    $progressColor = 'bg-warning';
+                                } else {
+                                    $progressColor = 'bg-success';
+                                }
+                                ?>
 
  <tr>
      <td><?php echo $position; ?></td>
      <td><?php echo $agence_nom; ?></td>
      <td><?php echo number_format($CA_prevision, 0, '.', ' '); ?>€</td>
-     <td><?php echo number_format($objectif, 0, '.', ' '); ?>€</td> <!-- Affichage de l'objectif -->
      <td>
          <div class="d-flex justify-content-between align-items-center gap-3">
              <div class="progress w-100" style="height: 10px">
@@ -51,7 +50,6 @@ if ($CA_prevision < 30000) {
  </tr>
 
  <?php
-$position++;
-}
-
+                                $position++;
+                            }
                             ?>
