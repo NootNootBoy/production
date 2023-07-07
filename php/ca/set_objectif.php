aaedfb6748369717069a1f55fdfa2791741cdcf4
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-07 12:39:55 +0200

    Update set_objectif.php

--- php/ca/set_objectif.php
+++ php/ca/set_objectif.php
@@ -10,11 +10,12 @@ $objectif = $_POST['objectif'];
 
 // Récupérer l'ID de l'agence de l'utilisateur
 $stmt = $pdo->prepare("SELECT agence_id FROM users WHERE username = ?");
+$stmt->execute([$username]);
 $user = $stmt->fetch();
 $agenceId = $user['agence_id'];
 
 // Récupérer le mois actuel
-$mois = date("m.d.y"); // Premier jour du mois actuel
+$mois = date("Y-m-01"); // Premier jour du mois actuel
 
 $stmt = $pdo->prepare('
     INSERT INTO objectifs (agence_id, mois, objectif)
