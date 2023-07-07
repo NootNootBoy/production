08d2029b6e662f6455d3d115eca4674731967623
François-Xavier Fin <54895686+NootNootBoy@users.noreply.github.com>
2023-07-07 12:27:10 +0200

    Update set_objectif.php

--- php/ca/set_objectif.php
+++ php/ca/set_objectif.php
@@ -4,12 +4,24 @@ include '../db_connection.php';
 include '../notifications/notifications.php';
 
 $objectif = $_POST['objectif'];
+$username = $_SESSION['username'];
+
+// Récupérer l'ID de l'agence de l'utilisateur connecté
+$stmt = $pdo->prepare("SELECT agence_id FROM users WHERE username = ?");
+$stmt->execute([$username]);
+$user = $stmt->fetch();
+$agenceId = $user['agence_id'];
+
+// Récupérer le mois actuel
+$mois = date('Y-m');
 
 $stmt = $pdo->prepare('
     INSERT INTO objectifs (agence_id, mois, objectif)
-    VALUES (:agenceId, CURDATE(), :objectif)
+    VALUES (:agenceId, :mois, :objectif)
     ON DUPLICATE KEY UPDATE objectif = :objectif
 ');
-$stmt->execute(['agenceId' => $agenceId, 'objectif' => $objectif]);
+$stmt->execute(['agenceId' => $agenceId, 'mois' => $mois, 'objectif' => $objectif]);
 
+header('Location: ../dashboard.php'); // Rediriger l'utilisateur vers la page du tableau de bord
+exit;
 ?>
